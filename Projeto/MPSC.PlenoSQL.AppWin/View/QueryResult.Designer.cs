872e1a22e88358bfd101fe61224778896eba7fa9
bruno.fernandes <bruno.fernandes@be5d293e-0630-5d45-9b40-789cb4ad01c5>
2015-10-01 18:26:03 +0000

    Remoção do mapa do edito de textos

--- Projeto/MPSC.PlenoSQL.AppWin/View/QueryResult.Designer.cs
+++ Projeto/MPSC.PlenoSQL.AppWin/View/QueryResult.Designer.cs
@@ -28,70 +28,47 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
-			this.scHorizontal = new System.Windows.Forms.SplitContainer();
-			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-			this.txtQuery = new FastColoredTextBoxNS.FastColoredTextBox();
-			this.dmMapaSQL = new FastColoredTextBoxNS.DocumentMap();
-			this.tcResultados = new System.Windows.Forms.TabControl();
-			this.tpMensagens = new System.Windows.Forms.TabPage();
-			this.txtMensagens = new System.Windows.Forms.TextBox();
-			this.tpDados = new System.Windows.Forms.TabPage();
-			this.dgResult = new MPSC.PlenoSQL.AppWin.View.DataGridViewOnDemand();
-			((System.ComponentModel.ISupportInitialize)(this.scHorizontal)).BeginInit();
-			this.scHorizontal.Panel1.SuspendLayout();
-			this.scHorizontal.Panel2.SuspendLayout();
-			this.scHorizontal.SuspendLayout();
-			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
-			this.splitContainer1.Panel1.SuspendLayout();
-			this.splitContainer1.Panel2.SuspendLayout();
-			this.splitContainer1.SuspendLayout();
-			((System.ComponentModel.ISupportInitialize)(this.txtQuery)).BeginInit();
-			this.tcResultados.SuspendLayout();
-			this.tpMensagens.SuspendLayout();
-			this.tpDados.SuspendLayout();
-			((System.ComponentModel.ISupportInitialize)(this.dgResult)).BeginInit();
-			this.SuspendLayout();
-			// 
-			// scHorizontal
-			// 
-			this.scHorizontal.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.scHorizontal.Location = new System.Drawing.Point(0, 0);
-			this.scHorizontal.Name = "scHorizontal";
-			this.scHorizontal.Orientation = System.Windows.Forms.Orientation.Horizontal;
-			// 
-			// scHorizontal.Panel1
-			// 
-			this.scHorizontal.Panel1.Controls.Add(this.splitContainer1);
-			// 
-			// scHorizontal.Panel2
-			// 
-			this.scHorizontal.Panel2.Controls.Add(this.tcResultados);
-			this.scHorizontal.Size = new System.Drawing.Size(441, 400);
-			this.scHorizontal.SplitterDistance = 300;
-			this.scHorizontal.TabIndex = 1;
-			this.scHorizontal.TabStop = false;
-			// 
-			// splitContainer1
-			// 
-			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.splitContainer1.Location = new System.Drawing.Point(0, 0);
-			this.splitContainer1.Name = "splitContainer1";
-			// 
-			// splitContainer1.Panel1
-			// 
-			this.splitContainer1.Panel1.Controls.Add(this.txtQuery);
-			// 
-			// splitContainer1.Panel2
-			// 
-			this.splitContainer1.Panel2.Controls.Add(this.dmMapaSQL);
-			this.splitContainer1.Size = new System.Drawing.Size(441, 300);
-			this.splitContainer1.SplitterDistance = 373;
-			this.splitContainer1.TabIndex = 1;
-			// 
-			// txtQuery
-			// 
-			this.txtQuery.AutoCompleteBracketsList = new char[] {
+            this.components = new System.ComponentModel.Container();
+            this.scHorizontal = new System.Windows.Forms.SplitContainer();
+            this.txtQuery = new FastColoredTextBoxNS.FastColoredTextBox();
+            this.tcResultados = new System.Windows.Forms.TabControl();
+            this.tpMensagens = new System.Windows.Forms.TabPage();
+            this.txtMensagens = new System.Windows.Forms.TextBox();
+            this.tpDados = new System.Windows.Forms.TabPage();
+            this.dgResult = new MPSC.PlenoSQL.AppWin.View.DataGridViewOnDemand();
+            ((System.ComponentModel.ISupportInitialize)(this.scHorizontal)).BeginInit();
+            this.scHorizontal.Panel1.SuspendLayout();
+            this.scHorizontal.Panel2.SuspendLayout();
+            this.scHorizontal.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtQuery)).BeginInit();
+            this.tcResultados.SuspendLayout();
+            this.tpMensagens.SuspendLayout();
+            this.tpDados.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // scHorizontal
+            // 
+            this.scHorizontal.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.scHorizontal.Location = new System.Drawing.Point(0, 0);
+            this.scHorizontal.Name = "scHorizontal";
+            this.scHorizontal.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // scHorizontal.Panel1
+            // 
+            this.scHorizontal.Panel1.Controls.Add(this.txtQuery);
+            // 
+            // scHorizontal.Panel2
+            // 
+            this.scHorizontal.Panel2.Controls.Add(this.tcResultados);
+            this.scHorizontal.Size = new System.Drawing.Size(425, 361);
+            this.scHorizontal.SplitterDistance = 270;
+            this.scHorizontal.TabIndex = 1;
+            this.scHorizontal.TabStop = false;
+            // 
+            // txtQuery
+            // 
+            this.txtQuery.AutoCompleteBracketsList = new char[] {
         '(',
         ')',
         '{',
@@ -102,130 +79,115 @@
         '\"',
         '\'',
         '\''};
-			this.txtQuery.AutoIndent = false;
-			this.txtQuery.AutoIndentCharsPatterns = "";
-			this.txtQuery.AutoScrollMinSize = new System.Drawing.Size(2, 16);
-			this.txtQuery.BackBrush = null;
-			this.txtQuery.CharHeight = 16;
-			this.txtQuery.CharWidth = 9;
-			this.txtQuery.CommentPrefix = "--";
-			this.txtQuery.Cursor = System.Windows.Forms.Cursors.IBeam;
-			this.txtQuery.DelayedTextChangedInterval = 100;
-			this.txtQuery.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
-			this.txtQuery.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.txtQuery.Font = new System.Drawing.Font("Courier New", 11.25F);
-			this.txtQuery.IsReplaceMode = false;
-			this.txtQuery.Language = FastColoredTextBoxNS.Language.SQL;
-			this.txtQuery.LeftBracket = '(';
-			this.txtQuery.Location = new System.Drawing.Point(0, 0);
-			this.txtQuery.Name = "txtQuery";
-			this.txtQuery.Paddings = new System.Windows.Forms.Padding(0);
-			this.txtQuery.RightBracket = ')';
-			this.txtQuery.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
-			this.txtQuery.Size = new System.Drawing.Size(373, 300);
-			this.txtQuery.TabIndex = 2;
-			this.txtQuery.Zoom = 100;
-			this.txtQuery.TextChanged += new System.EventHandler<FastColoredTextBoxNS.TextChangedEventArgs>(this.txtQuery_TextChanged);
-			this.txtQuery.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtQuery_KeyDown);
-			// 
-			// dmMapaSQL
-			// 
-			this.dmMapaSQL.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.dmMapaSQL.ForeColor = System.Drawing.Color.Maroon;
-			this.dmMapaSQL.Location = new System.Drawing.Point(0, 0);
-			this.dmMapaSQL.Name = "dmMapaSQL";
-			this.dmMapaSQL.Scale = 0.4F;
-			this.dmMapaSQL.Size = new System.Drawing.Size(64, 300);
-			this.dmMapaSQL.TabIndex = 0;
-			this.dmMapaSQL.Target = this.txtQuery;
-			this.dmMapaSQL.Text = "Mapa";
-			// 
-			// tcResultados
-			// 
-			this.tcResultados.Alignment = System.Windows.Forms.TabAlignment.Bottom;
-			this.tcResultados.Controls.Add(this.tpMensagens);
-			this.tcResultados.Controls.Add(this.tpDados);
-			this.tcResultados.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.tcResultados.Location = new System.Drawing.Point(0, 0);
-			this.tcResultados.Margin = new System.Windows.Forms.Padding(0);
-			this.tcResultados.Name = "tcResultados";
-			this.tcResultados.SelectedIndex = 0;
-			this.tcResultados.Size = new System.Drawing.Size(441, 96);
-			this.tcResultados.TabIndex = 1;
-			// 
-			// tpMensagens
-			// 
-			this.tpMensagens.Controls.Add(this.txtMensagens);
-			this.tpMensagens.Location = new System.Drawing.Point(4, 4);
-			this.tpMensagens.Margin = new System.Windows.Forms.Padding(0);
-			this.tpMensagens.Name = "tpMensagens";
-			this.tpMensagens.Size = new System.Drawing.Size(433, 70);
-			this.tpMensagens.TabIndex = 0;
-			this.tpMensagens.Text = "Mensagens";
-			this.tpMensagens.UseVisualStyleBackColor = true;
-			// 
-			// txtMensagens
-			// 
-			this.txtMensagens.AcceptsReturn = true;
-			this.txtMensagens.AcceptsTab = true;
-			this.txtMensagens.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.txtMensagens.Location = new System.Drawing.Point(0, 0);
-			this.txtMensagens.Margin = new System.Windows.Forms.Padding(0);
-			this.txtMensagens.Multiline = true;
-			this.txtMensagens.Name = "txtMensagens";
-			this.txtMensagens.ReadOnly = true;
-			this.txtMensagens.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-			this.txtMensagens.Size = new System.Drawing.Size(433, 70);
-			this.txtMensagens.TabIndex = 0;
-			this.txtMensagens.WordWrap = false;
-			// 
-			// tpDados
-			// 
-			this.tpDados.Controls.Add(this.dgResult);
-			this.tpDados.Location = new System.Drawing.Point(4, 4);
-			this.tpDados.Margin = new System.Windows.Forms.Padding(0);
-			this.tpDados.Name = "tpDados";
-			this.tpDados.Size = new System.Drawing.Size(433, 70);
-			this.tpDados.TabIndex = 1;
-			this.tpDados.Text = "Dados";
-			this.tpDados.UseVisualStyleBackColor = true;
-			// 
-			// dgResult
-			// 
-			this.dgResult.AllowUserToAddRows = false;
-			this.dgResult.AllowUserToDeleteRows = false;
-			this.dgResult.AllowUserToOrderColumns = true;
-			this.dgResult.AllowUserToResizeRows = false;
-			this.dgResult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.dgResult.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.dgResult.Location = new System.Drawing.Point(0, 0);
-			this.dgResult.Margin = new System.Windows.Forms.Padding(0);
-			this.dgResult.Name = "dgResult";
-			this.dgResult.ReadOnly = true;
-			this.dgResult.Size = new System.Drawing.Size(433, 70);
-			this.dgResult.TabIndex = 2;
-			this.dgResult.DataError += new System.Windows.Forms.DataGridViewDataErrorEventHandler(this.dgResult_DataError);
-			this.dgResult.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.dgResult_PreviewKeyDown);
-			// 
-			// QueryResult
-			// 
-			this.Controls.Add(this.scHorizontal);
-			this.Size = new System.Drawing.Size(441, 400);
-			this.scHorizontal.Panel1.ResumeLayout(false);
-			this.scHorizontal.Panel2.ResumeLayout(false);
-			((System.ComponentModel.ISupportInitialize)(this.scHorizontal)).EndInit();
-			this.scHorizontal.ResumeLayout(false);
-			this.splitContainer1.Panel1.ResumeLayout(false);
-			this.splitContainer1.Panel2.ResumeLayout(false);
-			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
-			this.splitContainer1.ResumeLayout(false);
-			((System.ComponentModel.ISupportInitialize)(this.txtQuery)).EndInit();
-			this.tcResultados.ResumeLayout(false);
-			this.tpMensagens.ResumeLayout(false);
-			this.tpMensagens.PerformLayout();
-			this.tpDados.ResumeLayout(false);
-			((System.ComponentModel.ISupportInitialize)(this.dgResult)).EndInit();
-			this.ResumeLayout(false);
+            this.txtQuery.AutoIndent = false;
+            this.txtQuery.AutoIndentCharsPatterns = "";
+            this.txtQuery.AutoScrollMinSize = new System.Drawing.Size(29, 16);
+            this.txtQuery.BackBrush = null;
+            this.txtQuery.CharHeight = 16;
+            this.txtQuery.CharWidth = 9;
+            this.txtQuery.CommentPrefix = "--";
+            this.txtQuery.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtQuery.DelayedTextChangedInterval = 100;
+            this.txtQuery.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+            this.txtQuery.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtQuery.Font = new System.Drawing.Font("Courier New", 11.25F);
+            this.txtQuery.IsReplaceMode = false;
+            this.txtQuery.Language = FastColoredTextBoxNS.Language.SQL;
+            this.txtQuery.LeftBracket = '(';
+            this.txtQuery.Location = new System.Drawing.Point(0, 0);
+            this.txtQuery.Name = "txtQuery";
+            this.txtQuery.Paddings = new System.Windows.Forms.Padding(0);
+            this.txtQuery.RightBracket = ')';
+            this.txtQuery.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.txtQuery.Size = new System.Drawing.Size(359, 270);
+            this.txtQuery.TabIndex = 2;
+            this.txtQuery.Zoom = 100;
+            this.txtQuery.TextChanged += new System.EventHandler<FastColoredTextBoxNS.TextChangedEventArgs>(this.txtQuery_TextChanged);
+            this.txtQuery.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtQuery_KeyDown);
+            // 
+            // tcResultados
+            // 
+            this.tcResultados.Alignment = System.Windows.Forms.TabAlignment.Bottom;
+            this.tcResultados.Controls.Add(this.tpMensagens);
+            this.tcResultados.Controls.Add(this.tpDados);
+            this.tcResultados.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tcResultados.Location = new System.Drawing.Point(0, 0);
+            this.tcResultados.Margin = new System.Windows.Forms.Padding(0);
+            this.tcResultados.Name = "tcResultados";
+            this.tcResultados.SelectedIndex = 0;
+            this.tcResultados.Size = new System.Drawing.Size(425, 87);
+            this.tcResultados.TabIndex = 1;
+            // 
+            // tpMensagens
+            // 
+            this.tpMensagens.Controls.Add(this.txtMensagens);
+            this.tpMensagens.Location = new System.Drawing.Point(4, 4);
+            this.tpMensagens.Margin = new System.Windows.Forms.Padding(0);
+            this.tpMensagens.Name = "tpMensagens";
+            this.tpMensagens.Size = new System.Drawing.Size(417, 61);
+            this.tpMensagens.TabIndex = 0;
+            this.tpMensagens.Text = "Mensagens";
+            this.tpMensagens.UseVisualStyleBackColor = true;
+            // 
+            // txtMensagens
+            // 
+            this.txtMensagens.AcceptsReturn = true;
+            this.txtMensagens.AcceptsTab = true;
+            this.txtMensagens.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtMensagens.Location = new System.Drawing.Point(0, 0);
+            this.txtMensagens.Margin = new System.Windows.Forms.Padding(0);
+            this.txtMensagens.Multiline = true;
+            this.txtMensagens.Name = "txtMensagens";
+            this.txtMensagens.ReadOnly = true;
+            this.txtMensagens.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtMensagens.Size = new System.Drawing.Size(417, 61);
+            this.txtMensagens.TabIndex = 0;
+            this.txtMensagens.WordWrap = false;
+            // 
+            // tpDados
+            // 
+            this.tpDados.Controls.Add(this.dgResult);
+            this.tpDados.Location = new System.Drawing.Point(4, 4);
+            this.tpDados.Margin = new System.Windows.Forms.Padding(0);
+            this.tpDados.Name = "tpDados";
+            this.tpDados.Size = new System.Drawing.Size(433, 70);
+            this.tpDados.TabIndex = 1;
+            this.tpDados.Text = "Dados";
+            this.tpDados.UseVisualStyleBackColor = true;
+            // 
+            // dgResult
+            // 
+            this.dgResult.AllowUserToAddRows = false;
+            this.dgResult.AllowUserToDeleteRows = false;
+            this.dgResult.AllowUserToOrderColumns = true;
+            this.dgResult.AllowUserToResizeRows = false;
+            this.dgResult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgResult.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgResult.Location = new System.Drawing.Point(0, 0);
+            this.dgResult.Margin = new System.Windows.Forms.Padding(0);
+            this.dgResult.Name = "dgResult";
+            this.dgResult.ReadOnly = true;
+            this.dgResult.Size = new System.Drawing.Size(433, 70);
+            this.dgResult.TabIndex = 2;
+            this.dgResult.DataError += new System.Windows.Forms.DataGridViewDataErrorEventHandler(this.dgResult_DataError);
+            this.dgResult.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.dgResult_PreviewKeyDown);
+            // 
+            // QueryResult
+            // 
+            this.ClientSize = new System.Drawing.Size(425, 361);
+            this.Controls.Add(this.scHorizontal);
+            this.Name = "QueryResult";
+            this.scHorizontal.Panel1.ResumeLayout(false);
+            this.scHorizontal.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.scHorizontal)).EndInit();
+            this.scHorizontal.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.txtQuery)).EndInit();
+            this.tcResultados.ResumeLayout(false);
+            this.tpMensagens.ResumeLayout(false);
+            this.tpMensagens.PerformLayout();
+            this.tpDados.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgResult)).EndInit();
+            this.ResumeLayout(false);
 
 		}
 
@@ -237,8 +199,6 @@
 		private System.Windows.Forms.TabPage tpDados;
 		private DataGridViewOnDemand dgResult;
 		private System.Windows.Forms.TextBox txtMensagens;
-		private System.Windows.Forms.SplitContainer splitContainer1;
-		private FastColoredTextBoxNS.FastColoredTextBox txtQuery;
-		private FastColoredTextBoxNS.DocumentMap dmMapaSQL;
+        private FastColoredTextBoxNS.FastColoredTextBox txtQuery;
 	}
 }
