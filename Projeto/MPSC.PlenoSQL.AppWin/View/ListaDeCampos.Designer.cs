ebedbfa84aae6e2ccc8a28e6175fbbc8ba7d8a91
Bruno Nogueira Fernandes <bruno.tere@gmail.com>
2016-02-05 15:16:49 +0000

    Refatoração Sem Relevância

--- Projeto/MPSC.PlenoSQL.AppWin/View/ListaDeCampos.Designer.cs
+++ Projeto/MPSC.PlenoSQL.AppWin/View/ListaDeCampos.Designer.cs
@@ -18,15 +18,14 @@ namespace MPSC.PlenoSQL.AppWin.View
 			// ListaDeCampos
 			// 
 			this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-			this.FormattingEnabled = true;
 			this.Name = "listBox";
 			this.ScrollAlwaysVisible = true;
 			this.Size = new System.Drawing.Size(300, 300);
 			this.DoubleClick += new System.EventHandler(this.Selecionar);
 			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.listBox_KeyDown);
+			this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ListaDeCampos_KeyPress);
 			this.Leave += new System.EventHandler(this.ListaDeCampos_Leave);
 			this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.ListaDeCampos_PreviewKeyDown);
-			this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ListaDeCampos_KeyPress);
 			this.ResumeLayout(false);
 
 		}
