8191c2fc84d738abe9d5da147429a046ada350c0
Bruno Nogueira Fernandes <bruno.tere@gmail.com>
2016-02-05 15:53:21 +0000

    Correção do numero de linhas afetadas

--- Projeto/MPSC.PlenoSQL.Editor/Properties/AssemblyInfo.cs
+++ Projeto/MPSC.PlenoSQL.Editor/Properties/AssemblyInfo.cs
@@ -13,6 +13,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("5ad576e5-a030-48cf-8cbc-42e5e6eb08fd")]
 
-[assembly: AssemblyVersion("1.0.0.503")]
-[assembly: AssemblyFileVersion("13.21.52.525")]
+[assembly: AssemblyVersion("1.0.0.507")]
+[assembly: AssemblyFileVersion("13.52.19.495")]
 [assembly: AssemblyInformationalVersion("1.0.2016.0205")]
--- Projeto/MPSC.PlenoSQL.Kernel/Util/Publicar.bat
+++ Projeto/MPSC.PlenoSQL.Kernel/Util/Publicar.bat
@@ -1,3 +1,6 @@
-﻿
+@Echo Off
+
+if Not Exist "\\10.21.25.59\transf\MTZ - SISTEMAS\bNogueira\PlenoSQL\" MkDir "\\10.21.25.59\transf\MTZ - SISTEMAS\bNogueira\PlenoSQL\"
+
 xCopy /Y "D:\Projeto\MP\MPSC.PlenoSQL\Projeto\Bin\*.dll" "\\10.21.25.59\transf\MTZ - SISTEMAS\bNogueira\PlenoSQL\*.*"
 xCopy /Y "D:\Projeto\MP\MPSC.PlenoSQL\Projeto\Bin\PlenoSQL.exe" "\\10.21.25.59\transf\MTZ - SISTEMAS\bNogueira\PlenoSQL\*.*"
