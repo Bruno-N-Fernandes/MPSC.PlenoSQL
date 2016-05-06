aca7c6aa711e59c2d7756131d9e53855aebca980
bruno.fernandes <bruno.fernandes@be5d293e-0630-5d45-9b40-789cb4ad01c5>
2016-05-06 18:34:30 +0000

    Implementação de formatação do SQL

---
+++ Projeto/MPSC.PlenoSQL.Kernel/Infra/FormatUtil.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace MPSC.PlenoSQL.Kernel.Infra
+{
+	public static class FormatUtil
+	{
+		public static String Embelezar(String sqlCode)
+		{
+			var sql = ToInLine(sqlCode);
+
+			return sql;
+		}
+
+		public static String ToInLine(String texto)
+		{
+			var mapa = new Dictionary<String, String>();
+			var retorno = Mapear(mapa, texto);
+
+			retorno = retorno.Replace("\r\n", " ");
+			retorno = retorno.Replace("\r", " ");
+			retorno = retorno.Replace("\n", " ");
+			retorno = retorno.Replace("\t", " ");
+			retorno = retorno.Trim();
+			retorno = Regex.Replace(retorno, "  *", " ");
+			retorno = retorno.Replace("( ", "(");
+			retorno = retorno.Replace(" )", ")");
+
+			foreach (var item in mapa)
+				retorno = retorno.Replace(item.Key, item.Value);
+
+			return retorno;
+		}
+
+		private static String Mapear(Dictionary<String, String> mapa, String texto)
+		{
+			var regex = "('[^']*')|(\"[^\"]*\")";
+
+			var matches = Regex.Matches(texto, regex);
+			foreach (Match match in matches)
+			{
+				var token = String.Format(@"#{{[({0})]}}#", mapa.Count);
+				var value = match.Captures[0].Value;
+				mapa[token] = value;
+				texto = texto.Replace(value, token);
+			}
+
+			return texto;
+		}
+	}
+}
---
+++ Projeto/MPSC.PlenoSQL.TestesUnitarios/Infra/TestandoFormatUtil.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MPSC.PlenoSQL.Kernel.Infra;
+using System;
+
+namespace MPSC.PlenoSQL.TestesUnitarios.Infra
+{
+	[TestClass]
+	public class TestandoFormatUtil
+	{
+
+		[TestMethod]
+		public void QuandoExecutaToInLine()
+		{
+			var retorno = FormatUtil.ToInLine(entrada);
+			Assert.AreEqual(modeloInLine, retorno);
+		}
+
+
+		[TestMethod]
+		public void QuandoExecutaToInLineESoTemAspasSimples()
+		{
+			var retorno = FormatUtil.ToInLine("   'Bruno'   'Nogueira'    'Fernandes'   'abc ");
+			Assert.AreEqual("'Bruno' 'Nogueira' 'Fernandes' 'abc", retorno);
+		}
+
+		[TestMethod]
+		public void QuandoExecutaToInLineESoTemAspasDuplas()
+		{
+			var retorno = FormatUtil.ToInLine(@"   ""Bruno""   ""Nogueira""    ""Fernandes""   ""abc ");
+			Assert.AreEqual(@"""Bruno"" ""Nogueira"" ""Fernandes"" ""abc", retorno);
+		}
+
+		[TestMethod]
+		public void QuandoExecutaToInLineETemAspasSimplesEAspasDuplas()
+		{
+			var retorno = FormatUtil.ToInLine(@"   'Bruno'   ""Nogueira""    'Fernandes'   ""abc' ");
+			Assert.AreEqual(@"'Bruno' ""Nogueira"" 'Fernandes' ""abc'", retorno);
+		}
+
+		[TestMethod]
+		public void QuandoExecutaToInLineETemAspasSimplesDentroDeAspasDuplas()
+		{
+			var retorno = FormatUtil.ToInLine(@"   ""'Bruno'""   ""Nogueira's""    "" e 'Fernandes' e ""   ""abc' ");
+			Assert.AreEqual(@"""'Bruno'"" ""Nogueira's"" "" e 'Fernandes' e "" ""abc'", retorno);
+		}
+
+		[TestMethod]
+		public void QuandoExecutaToInLineETemAspasDuplasDentroDeAspasSimples()
+		{
+			var retorno = FormatUtil.ToInLine(@"   '""Bruno""'   'Nogueira""s'    ' e ""Fernandes"" e '   'abc"" ");
+			Assert.AreEqual(@"'""Bruno""' 'Nogueira""s' ' e ""Fernandes"" e ' 'abc""", retorno);
+		}
+
+		private const String entrada = @"
+Select
+	T1.*
+From Tabela1 T1 
+Inner Join Tabela2 T2 On T2.T1Id = T1.Id
+Left Join View V On V.Id = T1.Id	
+Where (T1.Campo = @variavel) And   (	
+T2.Campo2 = 'teste   '   )	
+Order By 1;";
+
+
+		private const String modeloInLine = "Select T1.* From Tabela1 T1 Inner Join Tabela2 T2 On T2.T1Id = T1.Id Left Join View V On V.Id = T1.Id Where (T1.Campo = @variavel) And (T2.Campo2 = 'teste   ') Order By 1;";
+
+		private const String modeloFormatado = @"Select
+	T1.*
+From Tabela1 T1
+Inner Join Tabela2 T2 On T2.T1Id = T1.Id
+Left  Join View     V On V.Id = T1.Id
+Where (T1.Campo = @variavel)
+And ( T2.Campo2 = 'teste' )
+Order By 1;";
+
+
+	}
+}
