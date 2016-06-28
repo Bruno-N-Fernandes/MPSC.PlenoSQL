af94b65974367b200946301e1a795a14df8ecd52
bruno.fernandes <bruno.fernandes@be5d293e-0630-5d45-9b40-789cb4ad01c5>
2016-06-28 18:01:00 +0000

    Projeto Piloto MDA

---
+++ Projeto/MPSC.PlenoSQL.TestesUnitarios/MDA/MDD.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace MPSC.PlenoSQL.TestesUnitarios.MDA
+{
+	public class MDD
+	{
+		public class Modelo
+		{
+			public String Nome { get; set; }
+		}
+
+		public class Classe : Modelo
+		{
+			public List<Propriedade> Propriedades { get; set; }
+			public Classe(String texto)
+			{
+				Propriedades = new List<Propriedade>();
+				var linhas = texto.Split("\r\n;".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+				foreach (var linha in linhas)
+				{
+					Propriedades.Add(new Propriedade(linha));
+				}
+			}
+		}
+
+		public class Propriedade : Modelo
+		{
+			private static readonly Type[] types = Assembly.GetAssembly(typeof(String)).GetTypes().Where(t => t.IsVisible && t.IsPublic && t.IsSealed && !t.IsEnum && !t.IsGenericType && !t.IsGenericTypeDefinition && (t.FullName == "System." + t.Name)).OrderBy(t => t.Name).ToArray();
+			public Type Tipo { get; set; }
+
+			public Propriedade(String linha)
+			{
+				var prop = linha.Split(' ');
+				Tipo = Obter(prop[0]);
+				Nome = prop[1];
+			}
+			private Type Obter(String tipo)
+			{
+				return types.FirstOrDefault(t => t.Name == tipo)
+					?? types.FirstOrDefault(t => t.Name.ToUpper() == tipo.ToUpper())
+					?? types.FirstOrDefault(t => t.Name.StartsWith(tipo))
+					?? types.FirstOrDefault(t => t.Name.ToUpper().StartsWith(tipo.ToUpper()))
+					?? types.FirstOrDefault(t => t.Name.EndsWith(tipo))
+					?? types.FirstOrDefault(t => t.Name.ToUpper().EndsWith(tipo.ToUpper()))
+					?? types.FirstOrDefault(t => t.Name.Contains(tipo))
+					?? types.FirstOrDefault(t => t.Name.ToUpper().Contains(tipo.ToUpper()))
+
+					;
+			}
+		}
+
+		public static Classe Load(String texto)
+		{
+			return new Classe(texto);
+		}
+	}
+}
---
+++ Projeto/MPSC.PlenoSQL.TestesUnitarios/MDA/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MPSC.PlenoSQL.TestesUnitarios.MDA
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			var classe = MDD.Load(definition);
+			Assert.IsNotNull(classe);
+		}
+
+		public const String definition = @"
+int32 idade
+string nome
+datetime data
+string email
+";
+	}
+}
