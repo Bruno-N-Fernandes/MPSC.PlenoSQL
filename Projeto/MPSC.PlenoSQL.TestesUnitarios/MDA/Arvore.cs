1e5c1f67118878b7b2a14414f075cb672d6effed
bruno.fernandes <bruno.fernandes@be5d293e-0630-5d45-9b40-789cb4ad01c5>
2016-06-30 21:29:01 +0000

    Arvore

---
+++ Projeto/MPSC.PlenoSQL.TestesUnitarios/MDA/Arvore.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MPSC.PlenoSQL.TestesUnitarios.MDA
+{
+	public class Arvore<Tipo> where Tipo: IComparable
+	{
+		private No<Tipo> Raiz;
+
+		public void Adicionar(Tipo valor)
+		{
+			var no = new No<Tipo>(valor);
+			if (Raiz == null)
+				Raiz = no;
+
+			if (valor.CompareTo(Raiz.Valor) > 0)
+			{
+				no.Esquerda = Raiz;
+				Raiz = no;
+			}
+
+			if (valor.CompareTo(Raiz.Valor) > 0)
+			{
+				no.Direita = Raiz;
+				Raiz = no;
+			}
+		}
+	}
+
+	public class No<Tipo>
+	{
+		public No<Tipo> Esquerda;
+		public readonly Tipo Valor;
+		public No<Tipo> Direita;
+
+		public No(Tipo valor)
+		{
+			Valor = valor;
+		}
+	}
+}
