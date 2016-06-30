c9a6e1088eefbbf896038de7c27549172f44139a
Bruno Nogueira Fernandes <bruno.tere@gmail.com>
2016-06-30 17:05:32 +0000

    Fazendo funcionar como string

--- Projeto/MPSC.PlenoSQL.TestesUnitarios/MDA/TestandoMapa.cs
+++ Projeto/MPSC.PlenoSQL.TestesUnitarios/MDA/TestandoMapa.cs
@@ -18,29 +18,29 @@ namespace MPSC.PlenoSQL.TestesUnitarios.MDA
 		public void TestMethod2()
 		{
 			Console.WriteLine("Cadastro");
-			var dic = Mapa.De<Int32, Int32>(20)
-				.Add(0, 00)
-				.Add(1, 10)
-				.Add(2, 20)
-				.Add(3, 30)
-				.Add(4, 40)
-				.Add(5, 50)
-				.Add(6, 60)
-				.Add(7, 70)
-				.Add(8, 80)
-				.Add(9, 90)
+			var dic = Mapa.De<String, Int32>(20)
+				.Add("0", 00)
+				.Add("1", 10)
+				.Add("2", 20)
+				.Add("3", 30)
+				.Add("4", 40)
+				.Add("5", 50)
+				.Add("6", 60)
+				.Add("7", 70)
+				.Add("8", 80)
+				.Add("9", 90)
 				.Build();
 			Console.WriteLine("Pesquisa");
 
 			Assert.IsNotNull(dic);
-			Assert.AreEqual(00, dic[0]);
-			Assert.AreEqual(10, dic[1]);
-			Assert.AreEqual(20, dic[2]);
-			Assert.AreEqual(30, dic[3]);
-			Assert.AreEqual(40, dic[4]);
-			Assert.AreEqual(70, dic[7]);
-			Assert.AreEqual(80, dic[8]);
-			Assert.AreEqual(90, dic[9]);
+			Assert.AreEqual(00, dic["0"]);
+			Assert.AreEqual(10, dic["1"]);
+			Assert.AreEqual(20, dic["2"]);
+			Assert.AreEqual(30, dic["3"]);
+			Assert.AreEqual(40, dic["4"]);
+			Assert.AreEqual(70, dic["7"]);
+			Assert.AreEqual(80, dic["8"]);
+			Assert.AreEqual(90, dic["9"]);
 		}
 
 		[TestMethod, ExpectedException(typeof(InvalidOperationException))]
