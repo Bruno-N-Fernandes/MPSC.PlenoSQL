3c3999e8b184215bfcaab0d0dd69232aedfaeff2
Bruno Nogueira Fernandes <bruno.tere@gmail.com>
2016-07-01 22:05:06 +0000

    Implementando suporte para o MySql

---
+++ Projeto/MPSC.PlenoSQL.Kernel/Dados/BancoDeDadosMySql.cs
@@ -0,0 +1,46 @@
+﻿using FirebirdSql.Data.FirebirdClient;
+using MPSC.PlenoSQL.Kernel.Dados.Base;
+using MySql.Data.MySqlClient;
+using System;
+using System.ComponentModel;
+
+namespace MPSC.PlenoSQL.Kernel.Dados
+{
+	[DisplayName("My Sql")]
+	public class BancoDeDadosMySql : BancoDeDados<MySqlConnection>
+	{
+		protected override String StringConexaoTemplate { get { return @"Data Source={0};Initial Catalog={1};User ID={2};Password={3};"; } }
+		protected override String SQLSelectCountTemplate(String query) { return String.Format("Select Count(*) From ({0}) As ViewOfSelectCountFrom", query); }
+
+		protected override String SQLAllDatabases(String nome, Boolean comDetalhes) { throw new NotImplementedException("AllDatabasesSQL"); }
+		protected override String SQLAllProcedures(String nome, Boolean comDetalhes) { throw new NotImplementedException("SQLAllProcedures"); }
+		protected override String SQLTablesColumns
+		{
+			get
+			{
+				return @"
+Select
+    'T' As TipoTabela,
+    T.TABLE_NAME As NomeTabela,
+    T.TABLE_NAME As NomeInternoTabela,
+    C.COLUMN_NAME As NomeColuna,
+    CONCAT(
+        Case
+            When C.COLUMN_KEY = 'PRI' Then 'PK, '
+            When C.COLUMN_KEY = 'MUL' Then 'FK, '
+            Else ''
+        End,
+        C.COLUMN_TYPE, 
+        Case
+            When C.COLUMN_KEY = 'PRI' And EXTRA = 'auto_increment' Then ' Identity, NOT NULL'
+            When C.IS_NULLABLE = 'NO' Then ', NOT NULL'
+            Else ', NULL'
+        End
+   ) As DetalhesColuna
+From INFORMATION_SCHEMA.TABLES T
+Inner Join INFORMATION_SCHEMA.COLUMNS C On C.TABLE_NAME = T.TABLE_NAME
+Where (T.TABLE_TYPE = 'BASE TABLE')";
+			}
+		}
+	}
+}
--- Projeto/MPSC.PlenoSQL.TestesUnitarios/MDA/TestandoMapa.cs
+++ Projeto/MPSC.PlenoSQL.TestesUnitarios/MDA/TestandoMapa.cs
@@ -14,69 +14,6 @@ namespace MPSC.PlenoSQL.TestesUnitarios.MDA
 			Assert.IsNotNull(classe);
 		}
 
-		[TestMethod]
-		public void TestMethod2()
-		{
-			Console.WriteLine("Cadastro");
-			var dic = Mapa.De<String, Int32>(20)
-				.Add("0", 00)
-				.Add("1", 10)
-				.Add("2", 20)
-				.Add("3", 30)
-				.Add("4", 40)
-				.Add("5", 50)
-				.Add("6", 60)
-				.Add("7", 70)
-				.Add("8", 80)
-				.Add("9", 90)
-				.Build();
-			Console.WriteLine("Pesquisa");
-
-			Assert.IsNotNull(dic);
-			Assert.AreEqual(00, dic["0"]);
-			Assert.AreEqual(10, dic["1"]);
-			Assert.AreEqual(20, dic["2"]);
-			Assert.AreEqual(30, dic["3"]);
-			Assert.AreEqual(40, dic["4"]);
-			Assert.AreEqual(70, dic["7"]);
-			Assert.AreEqual(80, dic["8"]);
-			Assert.AreEqual(90, dic["9"]);
-		}
-
-		[TestMethod, ExpectedException(typeof(InvalidOperationException))]
-		public void TestMethod3()
-		{
-			var mapa =
-				Mapa.De<Int32, Int32>(10)
-				.Add(0, 00)
-				.Add(1, 10)
-				.Add(2, 20)
-				.Add(3, 30)
-				.Add(4, 40)
-				.Add(5, 50)
-				.Add(6, 60)
-				.Add(7, 70)
-				.Add(0, 80)
-				.Add(9, 90);
-			var dic = mapa.Build();
-		}
-
-		[TestMethod, ExpectedException(typeof(InvalidOperationException))]
-		public void TestMethod4()
-		{
-			var mapa =
-				Mapa.De<Int32, Int32>(5)
-				.Add(0, 00)
-				.Add(1, 10)
-				.Add(2, 20)
-				.Add(3, 30)
-				.Add(4, 40)
-				.Add(5, 50)
-				.Add(6, 60)
-				.Add(7, 70);
-			var dic = mapa.Build();
-		}
-
 
 		public const String definition = @"
 int32 idade
