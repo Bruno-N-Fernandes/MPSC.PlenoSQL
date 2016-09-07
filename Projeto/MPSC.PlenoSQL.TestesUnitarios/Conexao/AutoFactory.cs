8002648f33c532a6f1a7c029573df8330d93b542
Bruno Nogueira Fernandes <bruno.tere@gmail.com>
2016-09-07 22:45:47 +0000

    implemntando Query semelhante ao Dapper

---
+++ Projeto/MPSC.PlenoSQL.TestesUnitarios/Conexao/AutoFactory.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace MPSC.PlenoSQL.TestesUnitarios.Conexao
+{
+	public interface IConexao
+	{
+		IDataReader Executar(String cmdSql);
+	}
+
+	public class AutoFactory
+	{
+		private IConexao Conexao;
+		public AutoFactory(IConexao conexao)
+		{
+			Conexao = conexao;
+		}
+
+		public IEnumerable<TEntidade> Query<TEntidade>(String cmdSql)
+		{
+			return QueryImpl<TEntidade>(cmdSql).ToArray();
+		}
+
+		public IEnumerable<TEntidade> QueryImpl<TEntidade>(String cmdSql)
+		{
+			var reader = Executar(cmdSql);
+			while (reader.Read())
+				yield return New<TEntidade>(reader);
+		}
+
+		private TEntidade New<TEntidade>(IDataRecord dataRecord)
+		{
+			return Filler.New<TEntidade>(dataRecord);
+		}
+
+		private IDataReader Executar(String cmdSql)
+		{
+			return Conexao.Executar(cmdSql);
+		}
+	}
+}
---
+++ Projeto/MPSC.PlenoSQL.TestesUnitarios/Conexao/Torpedo.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace MPSC.PlenoSQL.TestesUnitarios.Conexao
+{
+	public class Torpedo
+	{
+		public Int64 Id { get; set; }
+		public DateTime Enviado { get; set; }
+	}
+
+
+	public abstract class Fill
+	{
+		public readonly Type Tipo;
+		private readonly Func<IDataRecord, Object> _fill;
+		protected Fill(Type tipo, Func<IDataRecord, Object> fill)
+		{
+			Tipo = tipo;
+			_fill = fill;
+		}
+
+		public TEntidade New<TEntidade>(IDataRecord dataRecord)
+		{
+			return (TEntidade)_fill(dataRecord);
+		}
+	}
+
+	public class Fill<TEntidade> : Fill
+	{
+		public Fill(Func<IDataRecord, Object> fill)
+			: base(typeof(TEntidade), fill)
+		{
+		}
+	}
+
+	public class Filler
+	{
+		public static readonly Fill[] fills;
+		static Filler()
+		{
+			fills = Inicializar().ToArray();
+		}
+
+		private static IEnumerable<Fill> Inicializar()
+		{
+			yield return new Fill<Torpedo>(d => new Torpedo { Id = (Int64)d["Id"], Enviado = (DateTime)d["Enviado"] });
+		}
+
+		public static TEntidade New<TEntidade>(IDataRecord dataRecord)
+		{
+			var tipo = typeof(TEntidade);
+			return fills.FirstOrDefault(f => f.Tipo == tipo).New<TEntidade>(dataRecord);
+		}
+	}
+}
