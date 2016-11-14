2fdfdbd4d9c305a556926728fe1b5b1c7fdffae5
Bruno Nogueira Fernandes <bruno.tere@gmail.com>
2016-11-14 19:05:08 +0000

    Melhorando classe base de factory da entidade

---
+++ Projeto/MPSC.PlenoSQL.TestesUnitarios/Conexao/AF/ClassFactoryAuto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+
+namespace MPSC.PlenoSQL.TestesUnitarios.Conexao.AF
+{
+	internal sealed class ClassFactoryAuto<TEntidade> : ClassFactory<TEntidade>
+	{
+		private readonly PropertyInfo[] properties;
+
+		internal ClassFactoryAuto()
+		{
+			properties = typeof(TEntidade).GetProperties().Where(p => p.CanWrite).ToArray();
+		}
+
+		public override TEntidade New(IDataRecord dataRecord)
+		{
+			var entidade = Activator.CreateInstance<TEntidade>();
+
+			foreach (var property in properties)
+			{
+				var value = GetValue<Object>(dataRecord, property.Name, null);
+				if (value != null) 
+					property.SetValue(entidade, value, null);
+			}
+
+			return entidade;
+		}
+	}
+}
