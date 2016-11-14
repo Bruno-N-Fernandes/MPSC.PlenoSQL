5b7cc17f4e0fe3925655aa4a5378c8deb8023ddd
Bruno Nogueira Fernandes <bruno.tere@gmail.com>
2016-11-14 21:50:05 +0000

    Fazendo testes de Métricas

--- Projeto/MPSC.PlenoSQL.TestesUnitarios/Conexao/TestandoAutoFactory.cs
+++ Projeto/MPSC.PlenoSQL.TestesUnitarios/Conexao/TestandoAutoFactory.cs
@@ -11,22 +11,73 @@ namespace MPSC.PlenoSQL.TestesUnitarios.Conexao
 	[TestClass]
 	public class TestandoAutoFactory
 	{
+		public const Int32 cTotal = 1000000;
+		private struct Metrica
+		{
+			public String Descricao;
+			public readonly DateTime DataHora;
+			public Metrica(String descricao)
+			{
+				Descricao = descricao;
+				DataHora = DateTime.Now;
+			}
+		}
+
 		[TestMethod]
 		public void TestMethod1()
 		{
-			//AutoFactory.Registrar<Torpedo, TorpedoFactory>();
-
+			var tempos = new List<Metrica>();
+			tempos.Add(new Metrica("Inicio"));
 			var autoFactory = new AutoFactory(new ConexaoFake());
-			var torpedos = autoFactory.Query<Torpedo>("Select * From Torpedo");
+			tempos.Add(new Metrica("CriarConexaoFake"));
 
-			Assert.IsNotNull(torpedos);
-			Assert.AreEqual(3, torpedos.Count());
+
+			var torpedos1 = autoFactory.Query<Torpedo>("Select * From Torpedo");
+			tempos.Add(new Metrica("Selecionar Com Factory Automatica"));
+			Assert.IsNotNull(torpedos1);
+			Assert.AreEqual(cTotal, torpedos1.Count());
+
+			tempos.Add(new Metrica("Assertions"));
+			AutoFactory.Registrar<Torpedo, TorpedoFactory>();
+			tempos.Add(new Metrica("Registar Factory"));
+
+			var torpedos2 = autoFactory.Query<Torpedo>("Select * From Torpedo");
+			tempos.Add(new Metrica("Selecionar Com Factory Manual"));
+			Assert.IsNotNull(torpedos2);
+			Assert.AreEqual(cTotal, torpedos2.Count());
+			tempos.Add(new Metrica("Assertions"));
+
+			var metricaAnterior = tempos[0];
+			foreach (var metrica in tempos.Skip(1))
+			{
+				Console.WriteLine("{0} = {1}", metrica.Descricao, metrica.DataHora - metricaAnterior.DataHora);
+				metricaAnterior = metrica;
+			}
 		}
 	}
 
 
 	public class ConexaoFake : IConexao
 	{
+		private static Dictionary<String, Object>[] _lista;
+
+		public ConexaoFake()
+		{
+			var lista = new List<Dictionary<String, Object>>();
+			for (Int64 id = 1; id <= TestandoAutoFactory.cTotal; id++)
+			{
+				lista.Add(
+					new Dictionary<String, Object>
+					{
+						{"Id", id},
+						{"Enviado", DateTime.Now},
+						{"Mensagem", "Mensagem"}
+					}
+				);
+			}
+			_lista = lista.ToArray();
+		}
+
 		public IDataReader Executar(String cmdSql)
 		{
 			return ObterTorpedo();
@@ -35,25 +86,7 @@ namespace MPSC.PlenoSQL.TestesUnitarios.Conexao
 
 		private IDataReader ObterTorpedo()
 		{
-			return new DataReaderFake(
-				new Dictionary<String, Object>
-				{
-					{"Id", 1},
-					{"Enviado", DateTime.Now}
-				}
-				,
-				new Dictionary<String, Object>
-				{
-					{"Id", 2},
-					{"Enviado", DateTime.Now}
-				}
-				,
-				new Dictionary<String, Object>
-				{
-					{"Id", 3},
-					{"Enviado", DateTime.Now}
-				}
-			);
+			return new DataReaderFake(_lista);
 		}
 	}
 }
