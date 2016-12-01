f9c1df6c9f8062b13be28a08e70572a16b16e285
Bruno Nogueira Fernandes <bruno.tere@gmail.com>
2016-12-01 18:44:22 +0000

    Criando estruturas de herança para os drivers do DB2

---
+++ Projeto/MPSC.PlenoSQL.Kernel/Dados/BancoDeDadosDb2.cs
@@ -0,0 +1,116 @@
+﻿using MPSC.PlenoSQL.Kernel.Dados.Base;
+using System;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+
+namespace MPSC.PlenoSQL.Kernel.Dados
+{
+	[DisplayName("Db2")]
+	public abstract class BancoDeDadosDb2<TDb2Connection> : BancoDeDados<TDb2Connection> where TDb2Connection : DbConnection, IDbConnection
+	{
+		protected override String SQLSelectCountTemplate(String query) { return String.Format("Select Count(*) From ({0}\r\n) As ViewOfSelectCountFrom", query); }
+
+		protected override String SQLAllDatabases(String nome, Boolean comDetalhes)
+		{
+			var detalhes = comDetalhes ? ", '' As Detalhes" : String.Empty;
+			var filtro = String.IsNullOrWhiteSpace(nome) ? String.Empty : " And (B.Schema_Name Like '" + nome + "%')";
+			return @"Select B.Schema_Name as Nome{0} From SYSIBM.Schemata B Where (B.Schema_Owner <> 'QSYS'){1}";
+		}
+
+		protected override String SQLTablesColumns { get { return QueryOf.cQueryCacheTablesColumns; } }
+
+		protected override String SQLAllProcedures(String nome, Boolean comDetalhes)
+		{
+			var detalhes = comDetalhes ? " || ' (' || External_name || ')'" : String.Empty;
+			var definicao = String.IsNullOrWhiteSpace(nome) ? ", '' as Detalhes" : ", Routine_Definition as Detalhes";
+			var filtro = String.IsNullOrWhiteSpace(nome) ? String.Empty : "And (Routine_Name = '" + nome + "')";
+			return String.Format(@"
+Select Routine_Name{0} As Nome {1}
+From SysIBM.Routines R
+Where (R.Routine_Type = 'PROCEDURE') {2}
+And (R.Specific_Schema = (values current schema))
+Order by Routine_Schema, Routine_Name", detalhes, definicao, filtro);
+		}
+
+		protected virtual String SQLAllSequences()
+		{
+			return @"
+Select
+	IFNull(S.Long_Comment, S.Sequence_Text) As Observacao,
+	(
+		Trim(S.Sequence_Schema) || '.' || Trim(S.Sequence_Name) || ' (' ||
+		Trim(S.System_Seq_Schema) || '.' || Trim(S.System_Seq_Name) ||')'
+	) As Sequence,
+	S.Sequence_Created,
+	S.Sequence_Definer
+From QSys2.SysSeqObjects S
+Where (S.Sequence_Schema = (values current schema))
+Order By S.Sequence_Name";
+		}
+
+		protected class QueryOf : Query
+		{
+			public const String cQueryCacheTablesColumns = @"
+Select
+	Tab.Table_Type As TipoTabela,
+	Tab.Table_Name As NomeTabela,
+	Tab.System_Table_Name As NomeInternoTabela,
+	Col.Column_Name as NomeColuna,
+	(
+		IfNull(
+			(Select
+				Case R.Constraint_Type
+					When 'PRIMARY KEY' Then 'PK, '
+					When 'FOREIGN KEY' Then 'FK, '
+					When 'UNIQUE' Then 'UQ, '
+					When 'CHECK' Then 'CK, '
+					Else 'IX, '
+				End
+			From SYSCSTCOL CC
+			Inner Join SYSCST R On (R.Constraint_Name = CC.Constraint_Name) And (R.Table_Name = CC.Table_Name)
+			Where (CC.Table_Name = Col.Table_Name) And (CC.Column_Name = Col.Column_Name)
+			And (R.Table_Name = Col.Table_Name) Fetch First 1 Row Only
+		), '') ||
+		Col.Data_Type ||
+		Case 
+			When (Col.Numeric_Scale Is Not Null) And (Col.Numeric_Scale > 0) Then  '(' || Cast(Col.Length as varchar(5)) || ', ' || Cast(Col.Numeric_Scale As VarChar(5)) || '), '
+			When Col.Data_Type Like '%CHAR%' Then '(' || Cast(Col.Length as varchar(10)) || '), '
+			Else ', '
+		End || 
+		Case When Col.Is_Nullable = 'Y' Then 'NULL' Else 'NOT NULL' End
+	) As DetalhesColuna
+From SysColumns Col
+Inner Join SysTables Tab On (Tab.System_Table_Name = Col.System_Table_Name)
+Where (Tab.Table_Type <> 'P')";
+		}
+
+	}
+}
+/*
+set schema <SchemaName>
+
+Select Replace(SQ.CmdSql, 'PUBLIC', 'TPSANTOS') As CmdSql
+From (
+    Select (
+            'GRANT EXECUTE ON' ||
+            ' ' || R.Routine_Type ||
+            ' ' || R.Routine_Schema ||
+            '.' || R.Routine_Name ||
+            ' TO ' || 'PUBLIC' || ';'
+        ) As CmdSql --, R.*
+    From SysIBM.Routines R Where (R.Routine_Schema = (values current schema)) And (R.Routine_Type In ('FUNCTION', 'PROCEDURE'))
+
+    Union All Select (
+            'GRANT USAGE ON' ||
+            ' ' || 'SEQUENCE' ||
+            ' ' || S.Sequence_Schema ||
+            '.' || S.Sequence_Name ||
+            ' TO ' || 'PUBLIC' || ';'
+        ) As CmdSql --, S.*
+    From QSys2.SysSeqObjects S
+    Where (S.Sequence_Schema = (values current schema))
+) As SQ
+
+Order By SQ.CmdSql;
+*/
--- Projeto/MPSC.PlenoSQL.Kernel/Dados/BancoDeDadosMsDb2.cs
+++ Projeto/MPSC.PlenoSQL.Kernel/Dados/BancoDeDadosMsDb2.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.HostIntegration.MsDb2Client;
-using MPSC.PlenoSQL.Kernel.Dados.Base;
 using MPSC.PlenoSQL.Kernel.Interface;
 using System;
 using System.ComponentModel;
@@ -7,95 +6,15 @@ using System.ComponentModel;
 namespace MPSC.PlenoSQL.Kernel.Dados
 {
 	[DisplayName("MsDb2-HIS")]
-	public class BancoDeDadosMsDb2 : BancoDeDados<MsDb2Connection>
+	public class BancoDeDadosMsDb2 : BancoDeDadosDb2<MsDb2Connection>
 	{
 		protected override String StringConexaoTemplate { get { return "Network Address={0};Initial Catalog={0};User ID={2};Password={3};Data Source={1};Database Name={1};Default Schema={1};Package Collection={1};Default Qualifier={1};Cache Authentication=False;Persist Security Info=True;Authentication=Server;Defer Prepare=False;DateTime As Char=False;Use Early Metadata=False;Derive Parameters=False;Network Transport Library=TCPIP;Host CCSID=37;PC Code Page=1252;Network Port=446;DBMS Platform=DB2/AS400;Process Binary as Character=False;DateTime As Date=False;AutoCommit=False;Connection Pooling=True;Units of Work=RUW;"; } }
-		protected override String SQLSelectCountTemplate(String query) { return String.Format("Select Count(*) From ({0}) As ViewOfSelectCountFrom", query); }
-
-		protected override String SQLAllDatabases(String nome, Boolean comDetalhes)
-		{
-			var detalhes = comDetalhes ? ", '' As Detalhes" : String.Empty;
-			var filtro = String.IsNullOrWhiteSpace(nome) ? String.Empty : " And (B.Schema_Name Like '" + nome + "%')";
-			return @"Select B.Schema_Name as Nome{0} From SYSIBM.Schemata B Where (B.Schema_Owner <> 'QSYS'){1}";
-		}
-
-		protected override String SQLTablesColumns { get { return QueryOf.cQueryCacheTablesColumns; } }
-
-		protected override String SQLAllProcedures(String nome, Boolean comDetalhes)
-		{
-			var detalhes = comDetalhes ? " || ' (' || External_name || ')'" : String.Empty;
-			var definicao = String.IsNullOrWhiteSpace(nome) ? ", '' as Detalhes" : ", Routine_Definition as Detalhes";
-			var filtro = String.IsNullOrWhiteSpace(nome) ? String.Empty : "And (Routine_Name = '" + nome + "')";
-			return String.Format(@"
-Select Routine_Name{0} As Nome {1}
-From SYSIBM.Routines
-Where (Routine_Type = 'PROCEDURE') {2}
-And (Specific_Schema = (values current schema))
-Order by Routine_Schema, Routine_Name", detalhes, definicao, filtro);
-		}
-
-		protected virtual String SQLAllSequences()
-		{
-			return @"
-Select
-	IFNull(Seq.Long_Comment, Seq.Sequence_Text) As Observacao,
-	(
-		Trim(Seq.Sequence_Schema) || '.' || Trim(Seq.Sequence_Name) || ' (' ||
-		Trim(Seq.System_Seq_Schema) || '.' || Trim(Seq.System_Seq_Name) ||')'
-	) As Sequence,
-	Seq.Sequence_Created,
-	Seq.Sequence_Definer
-From QSys2.SysSeqObjects Seq
-Where (Seq.Sequence_Schema = 'ESIM')
-Order By Seq.Sequence_Name";
-		}
-
 
 		public override IBancoDeDados Clone()
 		{
-            var iBancoDeDados = new BancoDeDadosMsDb2();
+			var iBancoDeDados = new BancoDeDadosMsDb2();
 			iBancoDeDados.ConfigurarConexao(_server, _dataBase, _usuario, _senha);
 			return iBancoDeDados;
 		}
-
-		protected class QueryOf : Query
-		{
-			public const String cQueryCacheTablesColumns = @"
-Select
-	Tab.Table_Type As TipoTabela,
-	Tab.Table_Name As NomeTabela,
-	Tab.System_Table_Name As NomeInternoTabela,
-	Col.Column_Name as NomeColuna,
-	(
-		IfNull(
-			(Select
-				Case R.Constraint_Type
-					When 'PRIMARY KEY' Then 'PK, '
-					When 'FOREIGN KEY' Then 'FK, '
-					When 'UNIQUE' Then 'UQ, '
-					When 'CHECK' Then 'CK, '
-					Else 'IX, '
-				End
-			From SysCstCol CC
-			Inner Join SYSCST R On (R.Constraint_Name = CC.Constraint_Name) And (R.Table_Name = CC.Table_Name)
-			Where (CC.Table_Name = Col.Table_Name) And (CC.Column_Name = Col.Column_Name)
-			And (R.Table_Name = Col.Table_Name) Fetch First 1 Row Only
-		), '') ||
-		Col.Data_Type ||
-		Case 
-			When (Col.Numeric_Scale Is Not Null) And (Col.Numeric_Scale > 0) Then  '(' || Cast(Col.Length as varchar(5)) || ', ' || Cast(Col.Numeric_Scale As VarChar(5)) || '), '
-			When Col.Data_Type Like '%CHAR%' Then '(' || Cast(Col.Length as varchar(10)) || '), '
-			Else ', '
-		End || 
-		Case When Col.Is_Nullable = 'Y' Then 'NULL' Else 'NOT NULL' End
-	) As DetalhesColuna
-From SysColumns Col
-Inner Join SysTables Tab On (Tab.System_Table_Name = Col.System_Table_Name)
-Where (Tab.Table_Type <> 'P')";
-		}
-
 	}
 }
-/*
-set schema <SchemaName>
-*/
