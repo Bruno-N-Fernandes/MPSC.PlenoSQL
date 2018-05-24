303dd5044809b614abb07c5102f8f6e08ed1814b
Bruno Nogueira Fernandes <bruno.tere@gmail.com>
2018-05-24 20:18:41 +0000

    Ajustes no script de versionamento e publicação

--- Projeto/MPSC.PlenoSQL.Kernel/Util/Publicar.bat
+++ Projeto/MPSC.PlenoSQL.Kernel/Util/Publicar.bat
@@ -1,6 +1,8 @@
 @Echo Off
 
-if Not Exist "\\svdatfs01\Sistemas\bNogueira\PlenoSQL\" MkDir "\\svdatfs01\Sistemas\bNogueira\PlenoSQL\"
+Set Destino=\\svdatfs01\Sistemas\bNogueira\PlenoSQL
 
-xCopy /Y "D:\Prj\MP\MPSC.PlenoSQL\Projeto\Bin\*.dll" "\\svdatfs01\Sistemas\bNogueira\PlenoSQL\*.*"
-xCopy /Y "D:\Prj\MP\MPSC.PlenoSQL\Projeto\Bin\PlenoSQL.exe" "\\svdatfs01\Sistemas\bNogueira\PlenoSQL\*.*"
+if Not Exist "%Destino%\" MkDir "%Destino%\"
+
+xCopy /Y "..\..\Bin\*.dll" "%Destino%\*.*"
+xCopy /Y "..\..\Bin\PlenoSQL.exe" "%Destino%\*.*"
